38ce6fc9920a31c7201373b618047a868c339d4b
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-07-13 10:51:46 -0700

    fix

--- git commit/commit_kyle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd D:/Project-499/Arduino
-git add --all
-timestamp() {
-  date +"at  %H:%M on %d/%m/%Y"
-}
-git commit -am "auto-commit : Update by Kyle $(timestamp)"
-git pull
-git push origin master
